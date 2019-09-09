57ab69ec78ea459581d76bc8cd1b279b54069c02
ruslan <random.ruslan@gmail.com>
2019-09-09 04:54:30 +0300

    Добавление вспомогательного файла: создание таблицы users

---
+++ extra/create_tables.py
@@ -0,0 +1,19 @@
+import sqlalchemy as sa
+
+from constants import DB_URL
+
+db = sa.create_engine(DB_URL)
+
+db.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username VARCHAR(64) UNIQUE NOT NULL,
+        gender VARCHAR(2) NOT NULL, 
+        preference VARCHAR(2));
+''')
+
+# try:
+#     db.execute("INSERT INTO users VALUES ('12345', 'my_first_user_in_table', 'm', 'b');")
+# except sa.exc.IntegrityError as e:
+#     pass
+
